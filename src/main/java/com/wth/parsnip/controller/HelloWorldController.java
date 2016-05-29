e301b91c06393db7742e318782251194eae17cb9
Chris Wilkinson <chris.wilkinson@csagroup.org>
2016-05-28 19:14:18 -0700

    initial commit of parsnip ap

---
+++ src/main/java/com/wth/parsnip/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.wth.parsnip.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
